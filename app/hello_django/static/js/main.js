a5db4b1f2b12f0ad32b5a942bc4a5aeccfc63d09
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-05-17 21:52:32 +0500

    nikita 25%

--- app/hello_django/static/js/main.js
+++ app/hello_django/static/js/main.js
@@ -1,20 +1,71 @@
 // JavaScript
-fetch(`http://localhost:8000/` + 'article', {
-  method: 'GET',
-  headers: {}
-})
-.then((response) => {
-  return response.json()
-})
-  .then((data) => {
+
+function createRecord(id, title, text, isFeatured){
     let cnt = document.createElement('div');
     cnt.classList.add('container');
-    cnt.setAttribute('id', data['id'])
+    cnt.setAttribute('id', id)
     cnt.innerHTML = `
-        <div class="title margin-right-15">${data['title']}</div>
-        <div class="text margin-right-15">${data['text']}</div>
-        <div class="isFeature margin-right-15">${data['isFeatured']}</div>
-        <button type="submit" class="btn-delete margin-right-15">Удалить</button>
-        <button type="submit" class="btn-change margin-right-15">Изменить</button>
+                    <div class="title margin-right-15">${title}</div>
+                    <div class="text margin-right-15">${text}</div>
+                    <div class="isFeature margin-right-15">${isFeatured}</div>
+                    <button type="submit" class="btn-delete margin-right-15" id="del-${id}">Удалить</button>
+                    <button type="submit" class="btn-change margin-right-15" id="chg-${id}">Изменить</button>
     `
-})
+    return cnt;
+}
+
+function getList() {
+    fetch(`http://localhost:8000/` + 'article', {
+        method: 'GET',
+        headers: {}
+    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            for (let k in data) {
+                document.body.append(createRecord(k, data[k]['title'], data[k]['text'], data[k]['isFeatured']))
+            }
+
+            let i = {
+                "1": {
+                    'title': "her",
+                    'text': 'her',
+                    'is': true
+                },
+                "2": {
+                    'title': "her",
+                    'text': 'her',
+                    'is': true
+                }
+            }
+        })
+}
+
+function createRecord(){
+    let btn = document.querySelector('.btn-create');
+    btn.onclick = function (){
+        fetch(`http://localhost:8000/` + 'article', {
+            method: 'POST',
+            body: {
+                'title': document.querySelector('input-title').value,
+                'text': document.querySelector('input-text').value,
+                'isFeatured': document.querySelector('input-isFeatured').value === 'true',
+            }
+        })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            for (let k in data) {
+                document.body.append(createRecord(k, data[k]['title'], data[k]['text'], data[k]['isFeatured']))
+            }
+        })
+    }
+}
+
+function patchRecord(){
+
+}
+getList();
+createRecord();
